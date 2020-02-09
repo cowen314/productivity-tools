a357ec92f5571af22597cab8e77a3f8604edafb1
Christian Owen <cowen314@gmail.com>
2020-02-09 13:53:57 -0600

    add basic parsing, clean up Toggl return types

--- toggl/main.py
+++ toggl/main.py
@@ -1,5 +1,7 @@
-import typing, requests
+import requests
+from typing import List, Dict
 from abc import abstractmethod
+import json
 
 
 class Toggl:
@@ -11,10 +13,11 @@ class Toggl:
         self.workspace_id = workspace_id
         self.user_agent = user_agent
 
-    def get_available_workspaces(self):
-        return requests.get(self._WORKSPACES_URL, auth=(self.api_key, self._PASSWORD))
+    def get_available_workspaces(self) -> List[Dict]:
+        response_text = requests.get(self._WORKSPACES_URL, auth=(self.api_key, self._PASSWORD)).text
+        return json.loads(response_text)
 
-    def get_entries_1_day(self, date: str):
+    def get_entries_1_day(self, date: str) -> Dict:
         """
         @param date: a string in the format 'YYYY-MM-DD'
         """
@@ -28,20 +31,65 @@ class Toggl:
             ),
             auth=(self.api_key, self._PASSWORD)
         )
-        return response
+        return json.loads(response.text)
+
+
+class TimeEntry:
+    def __init__(self, client_and_project: str = None, service_item: str = None, task: str = None, time_ms: int = None, description: str = None):
+        """
+        @param client_and_project: the client and project in the form <client>:<project>
+        """
+        self.client_and_project = client_and_project
+        self.service_item = service_item
+        self.task = task
+        self.time_ms = time_ms
+        self.description = description
 
 
 class _EntryParser:
     @abstractmethod
-    def parse_summary_entry_data(self, response: requests.Response):
+    def parse_summary_entry_data(self, daily_data: Dict):
+        """
+        @param daily_data: data for a single day
+        """
         raise NotImplementedError
 
 
 class ExampleParser(_EntryParser):
-    def parse_summary_entry_data(self, response: requests.Response):
-        pass
-    # TODO implement this
-    # TODO add some special parsing rules
+    def __init__(self, toggl_project_to_client_name_map: Dict[str, str] = {}):
+        self._toggl_project_to_client_name_map = toggl_project_to_client_name_map
+
+    def parse_summary_entry_data(self, daily_data: Dict):
+        time_entries = []
+        for project in daily_data['data']:
+            # get client name 
+            client_name = project['title']['project']  # I store client in toggl project name field
+            if client_name and client_name in self._toggl_project_to_client_name_map:  # sometimes, the client name in toggl is slightly different than the actual client name
+                client_name = self._toggl_project_to_client_name_map[client_name]
+
+            # loop through entries for that client
+            # for now, I'll just add a new entry for each entry in toggl
+            # at some point, might want to intelligently combine entries
+            for toggl_info in project['items']:
+                entry = TimeEntry()
+
+                toggl_entry_info = toggl_info['title']['time_entry'].split('|')
+                if len(toggl_entry_info) == 3:
+                    if client_name:
+                        entry.client_and_project = "%s:%s" % (client_name, toggl_entry_info[0])
+                    else:
+                        entry.client_and_project = toggl_entry_info[0]
+                    entry.service_item = toggl_entry_info[1]
+                    entry.description = toggl_entry_info[2]
+                elif len(toggl_entry_info) == 2:
+                    entry.client_and_project = ""
+                    entry.service_item = toggl_entry_info[0]
+                    entry.description = toggl_entry_info[1]
+                elif len(toggl_entry_info) == 1:
+                    entry.description = toggl_entry_info[0]
+                entry.time_ms = toggl_info['time']
+                time_entries.append(entry)
+        return time_entries
 
 
 def main():
@@ -51,7 +99,16 @@ def main():
         - a summary report that's grouped by project and subgrouped by time_entries should work (see https://github.com/toggl/toggl_api_docs/blob/master/reports/summary.md)
     - use gui automation tool to import time to timer
     """
-    Toggl()
+
+    # for example: 
+    api_key = ""  # SET THIS TO YOUR API KEY (e.g. "4b5c6d7e8b8c8e9e8e7b7a6a1a3c5b4a"). Log in then head to https://toggl.com/app/profile.
+    my_toggl = Toggl(api_key)
+    workspaces = my_toggl.get_available_workspaces()
+    print(workspaces)
+    my_toggl.workspace_id = workspaces[0]["id"]
+
+    wednesday_data = my_toggl.get_entries_1_day("2020-02-05")
+    print(wednesday_data)
 
 
 if __name__=="__main__":
