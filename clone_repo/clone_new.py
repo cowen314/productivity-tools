675f0db8dcd5d6e0bd88cc78dd597768e302fef9
Christian Owen <cowen314@gmail.com>
2020-11-14 02:35:38 -0600

    add outline of repo clone tool

---
+++ clone_repo/clone_new.py
@@ -0,0 +1,19 @@
+import argparse
+import subprocess
+import pathlib
+
+# set up command line interface
+
+# parse variables
+
+# clone some template repo into current repo (use subprocess)
+# remove the git dir
+# do.... other stuff??
+# clone the gitattributes template from the gitattributes template repo
+# hit the command to add a new repo to the remote
+# be sure to handle failures here
+# push up to the remote
+# 
+
+if __name__=="__main__":
+    pass
