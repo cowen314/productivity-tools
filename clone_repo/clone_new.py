b9e6d45caecc072510127f4275c950410d349b35
Christian Owen <christiano@dmcinfo.com>
2021-01-05 20:37:00 -0600

    add wizard to repo clone tool

--- clone_repo/clone_new.py
+++ clone_repo/clone_new.py
@@ -6,6 +6,7 @@ import shutil
 import urllib.parse
 import posixpath
 import datetime
+from sys import exit
 
 TEMP_DIR_NAME = ".repo-creator"
 CONFIG_FILENAME = "cloneconfig.json"
@@ -15,7 +16,14 @@ DEFAULT_CONFIG = {
     "default_dest_base_url": "https://git.dmcinfo.com/"
 }
 
-# TODO allow this to run in an interactive mode... like from a console window or something
+
+def exit_with_error(msg: str, running_as_wizard: bool):
+    if not running_as_wizard:
+        exit(str)
+    else:
+        input("FAIL: %s\nPress enter to exit." % msg)
+        exit()
+
 
 # read in config file, create a new one if none exists
 try:
@@ -29,20 +37,44 @@ except Exception as e:
 
 # set up command line interface
 parser = argparse.ArgumentParser()
-parser.add_argument('--project-name', help="Name of the new Gitlab project. Required when creating a new project/repo.", dest="project_name", required=True)
+parser.add_argument('--project-name', help="Name of the new Gitlab project. Required when creating a new project/repo.", dest="project_name")
 parser.add_argument('--namespace', help="Namespace of the project on Gitlab (default: %s)" % config["default_namespace"], default=config["default_namespace"])
 parser.add_argument('--template-url', help="Template repository to copy to the newly created repo (default: %s)" % config["default_template_url"], default=config["default_template_url"], dest="template_url")
 parser.add_argument('--target-path', help="Local path to copy files into", default=".", dest="target_path")
 parser.add_argument('--dest-base-url', help="URL base for the new Gitlab repo / project. This URL should not contain the namespace or project. (default: %s)" % config["default_dest_base_url"], default=config["default_dest_base_url"], dest="dest_base_url")
+parser.add_argument("--wizard", help="Run this tool with a wizard to walk through setting parameters.", action="store_true")
 
 # parse args
 args = parser.parse_args()
+if not args.wizard and not args.project_name:
+    exit_with_error("Must supply either a project name, or set the --wizard flag to true", args.wizard)
+
+# if the wizard flag was set, run the user through the wizard
+if args.wizard:
+    args.project_name = input("Enter the name of your repo / project: ")  # TODO add check for invalid chars
+    if args.project_name.strip() == "":
+        exit_with_error("", args.wizard)
+    ns = input("Enter the project namespace (leave blank for default: '%s'): " % config["default_namespace"])
+    if ns.strip() != "":
+        args.namespace = ns
+    tr = input("Enter the template repository to copy to the newly created repo (leave blank for default: '%s'): "
+               % config["default_template_url"])
+    if tr.strip() != "":
+        args.template_url = tr
+    bu = input("Enter the URL base for the new Gitlab repo / project. This URL should not contain the namespace or "
+               "project. (default: %s): " % config["default_dest_base_url"])
+    if bu.strip() != "":
+        args.dest_base_url = bu
+    tp = input("Enter the local path to copy files into (leave blank for this directory): ")
+    if tp.strip() != "":
+        args.target_path = tp
+
 target_path = pathlib.Path(args.target_path) / TEMP_DIR_NAME
-cleaned_project_name = args.project_name.replace(" ", "-")
+args.project_name = args.project_name.replace(" ", "-")  # clean the project name
 
 # check paths
 if target_path.exists() and len(list(target_path.glob("*"))) != 0:
-    exit("ERROR: Files were found in the target directory. This tool will only work on a blank directory.")
+    exit_with_error("ERROR: Files were found in the target directory. This tool will only work on a blank directory.", args.wizard)
 target_path.mkdir(parents=True, exist_ok=True)
 
 # clone some template repo into current repo (use subprocess), do it into a temp dir
@@ -52,7 +84,7 @@ po = subprocess.run(
     capture_output=True,
     cwd=target_path.resolve())
 if po.returncode > 0:
-    exit("ERROR: " + po.stderr.decode('ascii'))
+    exit_with_error("ERROR: " + po.stderr.decode('ascii'), args.wizard)
 elif len(po.stdout) > 0:
     print(po.stdout.decode('ascii'))
 
@@ -67,7 +99,7 @@ po = subprocess.run(
     cwd=repo_dir.resolve())
 if po.returncode > 0:
     # shutil.rmtree(repo_dir)
-    exit("ERROR: " + po.stderr.decode('ascii'))
+    exit_with_error("ERROR: " + po.stderr.decode('ascii'), args.wizard)
 elif len(po.stdout) > 0:
     print(po.stdout.decode('ascii'))
 
@@ -78,7 +110,7 @@ po = subprocess.run(
     cwd=repo_dir.resolve())
 if po.returncode > 0:
     # shutil.rmtree(repo_dir)
-    exit("ERROR: " + po.stderr.decode('ascii'))
+    exit_with_error("ERROR: " + po.stderr.decode('ascii'), args.wizard)
 elif len(po.stdout) > 0:
     print(po.stdout.decode('ascii'))
 
@@ -94,7 +126,7 @@ po = subprocess.run(
     cwd=repo_dir.resolve())
 if po.returncode > 0:
     # shutil.rmtree(repo_dir)
-    exit("ERROR: Failed to create new repository (does it already exist?)\n" + po.stderr.decode('ascii'))
+    exit_with_error("ERROR: Failed to create new repository (does it already exist?)\n" + po.stderr.decode('ascii'), args.wizard)
 elif len(po.stdout) > 0:
     print(po.stdout.decode('ascii'))
 
@@ -105,7 +137,7 @@ po = subprocess.run(
     cwd=repo_dir.resolve())
 if po.returncode > 0:
     # shutil.rmtree(repo_dir)
-    exit("ERROR: " + po.stderr.decode('ascii'))
+    exit_with_error("ERROR: " + po.stderr.decode('ascii'), args.wizard)
 elif len(po.stdout) > 0:
     print(po.stdout.decode('ascii'))
 
@@ -114,14 +146,14 @@ print("Adding initial commit")
 init_file = repo_dir / "README.md"
 init_file.touch()
 with open(init_file, "a") as fh:
-    fh.write("\nThis project was created %s by copying %s." % (str(datetime.datetime.now()), args.template_url))
+    fh.write("\n\n---\n\nThis project was created %s by copying %s." % (str(datetime.datetime.now()), args.template_url))
 po = subprocess.run(
-    ["git", "commit", "-am", "'REPO CREATOR: INIT'"],
+    ["git", "commit", "-am", "REPO CREATOR: INIT"],
     capture_output=True,
     cwd=repo_dir.resolve())
 if po.returncode > 0:
     # shutil.rmtree(repo_dir)
-    exit("ERROR: " + po.stderr.decode('ascii'))
+    exit_with_error("ERROR: " + po.stderr.decode('ascii'), args.wizard)
 elif len(po.stdout) > 0:
     print(po.stdout.decode('ascii'))
 
@@ -132,12 +164,12 @@ po = subprocess.run(
     cwd=repo_dir.resolve())
 if po.returncode > 0:
     # shutil.rmtree(repo_dir)
-    exit("ERROR: " + po.stderr.decode('ascii'))
+    exit_with_error("ERROR: " + po.stderr.decode('ascii'), args.wizard)
 elif len(po.stdout) > 0:
     print(po.stdout.decode('ascii'))
 
 print("Moving out of temporary directory")
-repo_dir.rename(repo_dir.parent.parent / cleaned_project_name)  # move up a directory, into appropriately named folder
+repo_dir.rename(repo_dir.parent.parent / args.project_name)  # move up a directory, into appropriately named folder
 (repo_dir.parent.parent / TEMP_DIR_NAME).rmdir()
 
 new_project_url = urllib.parse.urljoin(args.dest_base_url, posixpath.join(args.namespace, args.project_name))
