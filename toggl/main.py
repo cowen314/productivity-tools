ec2c34172a61c7e49a0c125c489ab51a01844fe9
Christian Owen <christiano@dmcinfo.com>
2021-06-17 12:35:55 -0500

    update API URLs per message from toggl, still needs to be tested

--- toggl/main.py
+++ toggl/main.py
@@ -4,8 +4,8 @@ from abc import abstractmethod
 
 
 class Toggl:
-    _BASE_SUMMARY_URL = "https://toggl.com/reports/api/v2/summary"
-    _WORKSPACES_URL = "https://www.toggl.com/api/v8/workspaces"
+    _BASE_SUMMARY_URL = "https://api.track.toggl.com/reports/api/v2/summary"
+    _WORKSPACES_URL = "https://www.api.track.toggl.com/api/v8/workspaces"
     _PASSWORD = "api_token"
 
     def __init__(self, api_key: str, workspace_id: int = None, user_agent: str = "api_test"):
