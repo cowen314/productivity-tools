c2984b928a4de3a4e629e5119d1aa328e14f3683
Christian Owen <cowen314@gmail.com>
2022-10-29 17:00:41 -0500

    add invoice generation entry point

--- toggl_importer.py
+++ toggl_importer.py
@@ -15,6 +15,7 @@ class ImportType(Enum):
 app = typer.Typer()
 
 
+@app.command()
 def pull_and_import_one_day(date: date, api_key: Optional[str], import_type: ImportType = ImportType.TEXT_DUMP):
     if api_key is None:
         toggl_secrets = get_toggl_secrets()
@@ -28,3 +29,12 @@ def pull_and_import_one_day(date: date, api_key: Optional[str], import_type: Imp
         raise ValueError("Unsupported importer specified")
 
     pull_and_import_single_day(f"{date.year:04}-{date.month:02}-{date.day:02}", api_key, importer=importer)
+
+
+@app.command()
+def generate_invoice_model(start_date: date, end_date: date, api_key: Optional[str]):
+    if api_key is None:
+        toggl_secrets = get_toggl_secrets()
+        api_key = toggl_secrets.api_key
+    
+    
