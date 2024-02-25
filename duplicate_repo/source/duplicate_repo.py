8865672413bdeec255f26ff84b9d71982aff835e
Fadil Eledath <fadile@dmcinfo.com>
2024-02-25 10:22:19 -0600

    Add SSH support

--- duplicate_repo/source/duplicate_repo.py
+++ duplicate_repo/source/duplicate_repo.py
@@ -7,6 +7,7 @@ import urllib.parse
 import posixpath
 import datetime
 import sys
+import re
 from version import version
 from repo_parameters_ui import ToolUI
 from urllib.parse import urlparse
@@ -18,7 +19,8 @@ CONFIG_FILENAME = "cloneconfig.json"  # FIXME this currently goes into the clone
 DEFAULT_CONFIG = {
     "default_namespace": "DMC/labview/",
     "default_template_url": "https://git.dmcinfo.com/DMC/labview/dmc-templates/labview-template-project.git",
-    "default_dest_base_url": "https://git.dmcinfo.com/"
+    "default_dest_base_url": "https://git.dmcinfo.com/",
+    "default_protocol": "HTTPS"
 }
 if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):  # if in the context of a PyInstaller bundled application
     EXE_DIR = Path(sys._MEIPASS)
@@ -56,6 +58,22 @@ def clean_url_string(url: str) -> str:
     return url
 
 
+def apply_protocol(url: str, protocol: str, wizard):
+    if protocol not in ["HTTPS", "SSL"]:
+        exit_with_error(f" > ERROR: Invalid protocol specified - {protocol}", wizard)
+    match = re.search(r'^(git@|https?:\/\/)([^\/:]*)[:\/](.*)', url) # Split URL into protocol, host, and the rest
+    if match is None:
+        exit_with_error(f" > ERROR: Invalid URL format - {url}", wizard)
+    _protocol_str, host, extension = match.groups()
+    if protocol == "HTTPS":
+        formatted = f"https://{host}/{extension}"
+    else:
+        formatted = f"git@{host}:{extension}"
+    if not formatted.endswith(".git"):
+        formatted += ".git"
+    return formatted
+
+
 # read in config file, create a new one if none exists
 CONFIG_PATH = Path.cwd() / EXE_DIR / CONFIG_FILENAME
 try:
@@ -77,6 +95,7 @@ parser.add_argument('--namespace', help="Namespace of the project on Gitlab (def
 parser.add_argument('--template-url', help="Template repository to copy to the newly created repo (default: %s)" % config["default_template_url"], default=config["default_template_url"], dest="template_url")
 parser.add_argument('--target-path', help="Local path to copy files into", default=".", dest="target_path")
 parser.add_argument('--dest-base-url', help="URL base for the new Gitlab repo / project. This URL should not contain the namespace or project. (default: %s)" % config["default_dest_base_url"], default=config["default_dest_base_url"], dest="dest_base_url")
+parser.add_argument('--protocol', help="Protocol to use for git repository. Either HTTPS or SSL. (default: %s)" % config["default_protocol"], default=config["default_protocol"], dest="protocol")
 parser.add_argument("--wizard", help="Run this tool with a wizard to walk through setting parameters.", action="store_true")
 
 # parse args
@@ -87,7 +106,7 @@ if not args.project_name:
 
 # if the wizard flag was set, run the user through the wizard
 if args.wizard:
-    toolUI = ToolUI(args.namespace, args.template_url, args.dest_base_url, args.target_path)
+    toolUI = ToolUI(args.namespace, args.template_url, args.dest_base_url, args.target_path, args.protocol)
     toolUI.launchUI()
     if toolUI.project_name == "":
         exit_with_error(" > Invalid project name", args.wizard)
@@ -96,6 +115,7 @@ if args.wizard:
     args.template_url = toolUI.template_url
     args.dest_base_url = toolUI.dest_base_url
     args.target_path = toolUI.target_path
+    args.protocol = toolUI.protocol
 
     print("---Parameter Setting Completed---")
     print("Project Name: %s" % toolUI.project_name)
@@ -103,6 +123,7 @@ if args.wizard:
     print("Template URL: %s" % toolUI.template_url)
     print("Dest Base URL: %s" % toolUI.dest_base_url)
     print("Target Path: %s" % toolUI.target_path)
+    print("Protocol: %s" % toolUI.protocol)
 
     # args.project_name = input(" > Enter the name of your repo / project: ")  # TODO add check for invalid chars
     # if args.project_name.strip() == "":
@@ -131,7 +152,8 @@ if temporary_dir.exists() and len(list(temporary_dir.glob("*"))) != 0:
 temporary_dir.mkdir(parents=True, exist_ok=True)
 
 # clone some template repo into current repo (use subprocess), do it into a temp dir
-call_cli(f"git clone {clean_url_string(args.template_url)}", args.wizard, temporary_dir.resolve(), pre_msg=f"Cloning template from {clean_url_string(args.template_url)}")
+clone_url = apply_protocol(clean_url_string(args.template_url), args.protocol, args.wizard)
+call_cli(f"git clone {clone_url}", args.wizard, temporary_dir.resolve(), pre_msg=f"Cloning template from {clone_url}")
 
 # grab repo dir
 repo_dir = list(temporary_dir.glob("*"))[0]  # there should only be one folder at the target path after the clone
@@ -148,7 +170,9 @@ call_cli("git remote remove origin", args.wizard, repo_dir.resolve(), pre_msg="R
 # hit the command to add a new repo to the remote
 # (https://docs.gitlab.com/ee/gitlab-basics/create-project.html#push-to-create-a-new-project)
 namespace_url = urllib.parse.urljoin(clean_url_string(args.dest_base_url), args.namespace)
+namespace_url = apply_protocol(namespace_url, args.protocol, args.wizard)
 new_project_url = urllib.parse.urljoin(clean_url_string(args.dest_base_url), posixpath.join(args.namespace, args.project_name+".git"))
+new_project_url = apply_protocol(new_project_url, args.protocol, args.wizard)
 
 call_cli("git push --set-upstream %s master" % new_project_url, args.wizard, repo_dir.resolve(),
         pre_msg="Pushing to new project at %s" % new_project_url,
@@ -181,11 +205,10 @@ call_cli("git push", args.wizard, repo_dir.resolve(), pre_msg="Pushing")
 print(" > Moving out of temporary directory")
 repo_dir.rename(repo_dir.parent.parent / args.project_name)  # move up a directory, into appropriately named folder
 
-new_project_url = urllib.parse.urljoin(clean_url_string(args.dest_base_url), posixpath.join(args.namespace, args.project_name))
 project_settings_url = urllib.parse.urljoin(clean_url_string(args.dest_base_url), posixpath.join(args.namespace, args.project_name, "edit"))
 print(" > SUCCESS: Repository created successfully!"
       "\n > URL of new project is %s."
       "\n > Project visibility is currently set to private, navigate to %s to update it."
-      "\n > All code sourced from %s" % (new_project_url, project_settings_url, clean_url_string(args.template_url)))
+      "\n > All code sourced from %s" % (new_project_url[:-4], project_settings_url, clean_url_string(args.template_url)))
 if args.wizard:
     input(" > Press enter to exit")
